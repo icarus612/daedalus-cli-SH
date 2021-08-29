ad83d9dc41b8be4c01e3761c482f71b299c1b49c
icarus612 <ellishogan@gmail.com>
2021-08-29 16:30:08 -0400

    updated

--- .daedalus.sh
+++ .daedalus.sh
@@ -9,12 +9,18 @@ crs() {
 			g++ $2.cpp -o $2.out && ./$2.out
 			;;
 		gpush)
-			local msg="$2"
+
+			local msg="updated"
+			if [ $# -gt "2" ]
+				then
+					$msg=$2
+			fi
+			
 			echo $msg
 			git add . && git commit -m $msg && git push
 			;;
 		*)
-			echo "No command found by name $2"
+			echo "Command '$1' not found. \n"
 			;;
 	esac
 
--- cpp.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream> 
- int main() {
-	std::cout << "No ellis is (the bitch)" << std::endl;
-
-	return 0;
- }
